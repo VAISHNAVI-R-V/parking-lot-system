e98883f7993a980c584f574da9557d084c583bb9
VAISHNAVI-R-V <vaishucv14@gmail.com>
2021-11-11 05:37:27 +0530

    ADDED: Test Cases for UNPARKED Vehicles.

--- src/main/java/com/bridgelabz/ParkingLotSystem.java
+++ src/main/java/com/bridgelabz/ParkingLotSystem.java
@@ -20,13 +20,27 @@ public class ParkingLotSystem {
     /**
      * Purpose : To Park the Vehicle in Parking Lot
      *
-     * @param vehicle given vehicle as parameter
-     * @return True For Vehicle Parked
+     * @param vehicle : takes vehicle as parameter
+     * @return True : For Vehicle Parked
      */
     public boolean parkVehicle(Object vehicle) {
         if (this.vehicle != null)
             return false;
         this.vehicle = vehicle;
         return true;
+    }
+
+    /**
+     * Purpose : To UnParked the Vehicle from parking lot
+     *
+     * @param vehicle : takes vehicle as parameter
+     * @return  : Vehicle UnParked
+     */
+    public boolean unParkVehicle(Object vehicle) {
+        if (this.vehicle.equals(vehicle)) {
+            this.vehicle = null;
+            return true;
+        }
+        return false;
     }
 }
--- src/test/java/com/bridgelabz/ParkingLotSystemTest.java
+++ src/test/java/com/bridgelabz/ParkingLotSystemTest.java
@@ -21,17 +21,25 @@ public class ParkingLotSystemTest {
 
     @Test
     void givenAVehicle_WhenParked_ShouldReturnTrue() {
-        Vehicle vehicle = new Vehicle("KA-2580", "Black", "Audi");
+        Vehicle vehicle1 = new Vehicle("KA-2580", "Black", "Audi");
         boolean isParked = parkingLotSystem.parkVehicle(new Object());
         Assertions.assertTrue(isParked);
     }
 
     @Test
     public void givenAVehicle_WhenAlreadyParked_ShouldReturnFalse() {
-        Vehicle vehicle = new Vehicle("KA-2580", "Black", "Audi");
+        Vehicle vehicle1 = new Vehicle("KA-2580", "Black", "Audi");
         parkingLotSystem.parkVehicle(vehicle);
         boolean isParked = parkingLotSystem.parkVehicle(new Object());
         Assertions.assertFalse(isParked);
+    }
+
+    @Test
+    public void givenAVehicle_WhenUnParked_ShouldReturnTrue() {
+        Vehicle vehicle1 = new Vehicle("KA-3280", "Silver", "KIA");
+        parkingLotSystem.parkVehicle(vehicle);
+        boolean isUnParked = parkingLotSystem.unParkVehicle(vehicle);
+        Assertions.assertTrue(isUnParked);
     }
 }
 
